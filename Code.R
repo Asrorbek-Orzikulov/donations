fc09a4dee227682f0542400b2ff916e0aeefe3bf
“Asrorbek-Orzikulov” <asrorbek.arzikulov@gmail.com>
2021-05-16 17:04:18 +0200

    Adding transition matrix.

--- Code.R
+++ Code.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 library(readxl)
 library(lubridate)
+library(e1071)
+
 rm(list = ls())
 setwd("/Users/asrorbek/Desktop/Master 1/Semester 3/Hackathon")
 contacts <- read_excel("Contacts.xlsx")
@@ -64,6 +66,7 @@ for (table_name in tables_to_read) {
 }
 
 print(107829 + 110170 + 150222 + 144844 + 152435 == nrow(donors))
+rm(table)
 
 view(head(donors, 20))
 view(tail(donors, 50))
@@ -102,8 +105,14 @@ donors %>%
   head()
 
 # frequency column
+donors %>%
+  filter(`ventilation amount` == 0) %>%
+  head(30) %>%
+  view()
+
 donors <- donors %>%
   ungroup() %>%
+  filter(`ventilation amount` > 0) %>%
   group_by(`contact ID`, year_donation) %>%
   add_tally() %>%
   rename(freq = n) %>%
@@ -117,13 +126,14 @@ donors <- donors %>%
 
 # keeping the most recent donation
 donors_csv <- donors %>% 
-  group_by(`contact ID`, year_donation) %>%
+  group_by(year_donation, `contact ID`) %>%
   filter(recency == min(recency)) %>%
-  select(`contact ID`, `cam code`, `oft code`, `payment method`,
+  slice_max(order_by = `ventilation amount`, with_ties = FALSE) %>%
+  select(`contact ID`, `cam code`, `oft code`, `payment method`, RF,
          year_donation, recency:monetary_value)
 
 # quick check of donors_csv
-nrow(donors_csv) == 115108
+nrow(donors_csv) == 111105
 view(head(donors_csv, 30))
 view(tail(donors_csv, 30))
 
@@ -136,13 +146,72 @@ donors %>%
          year_donation == 2018) %>%
   view()
 
+# should be 10458303
+donors %>% ungroup() %>% summarise(sum = sum(`ventilation amount`))
+donors_csv %>% ungroup() %>% summarise(sum = sum(monetary_value))
+sum(donors[["ventilation amount"]], na.rm = TRUE) == sum(donors_csv$monetary_value)
+
+# plotting and transformation: recency
+hist(donors_csv$recency)
+skewness(donors_csv$recency)
+hist(donors_csv$recency^(1/3))  # best one
+skewness(donors_csv$recency^(1/3))
+hist(log(donors_csv$recency + 1))
+skewness(log(donors_csv$recency + 1))
+
+# plotting and transformation: freq
+hist(donors_csv$freq)
+skewness(donors_csv$freq)
+hist(sqrt(donors_csv$freq))
+skewness(sqrt(donors_csv$freq))
+hist(log(donors_csv$freq))
+skewness(log(donors_csv$freq))  # best one
+
+# plotting and transformation: monetary_value
+hist(donors_csv$monetary_value)
+skewness(donors_csv$monetary_value)
+hist(sqrt(donors_csv$monetary_value))
+skewness(sqrt(donors_csv$monetary_value))
+hist(log(donors_csv$monetary_value))
+skewness(log(donors_csv$monetary_value)) # best one
+
+# scaling and transforming variables
+donors_csv <- donors_csv %>%
+  group_by(year_donation) %>% 
+  mutate(
+    recency = recency^(1/3),
+    recency = (recency - mean(recency)) / sd(recency),
+    freq = sqrt(freq),
+    freq = (freq - mean(freq)) / sd(freq),
+    monetary_value = log(monetary_value),
+    monetary_value = (monetary_value - mean(monetary_value)) / sd(monetary_value)
+         )
+
+donors_csv <- donors_csv %>%
+  mutate(
+    rf_new = case_when(
+      RF == "Oui" | RF == "yes" ~ 1,
+      RF == "Non" | RF == "no" ~ 0
+    )
+  ) %>%
+  ungroup()
+
 # exporting the data
+write_csv(donors, "donors_united.csv")
 write_csv(donors_csv, "donors_new.csv")
 
-# plotting
-hist(donors_csv$recency)
-hist(donors_csv$freq)
-hist(donors_csv$monetary_value)
+names(donors_csv)
+donors_csv %>%
+  select(`contact ID`:`payment method`, rf_new) %>%
+  write_csv("descriptors.csv")
 
+donors_csv %>%
+  select(`contact ID`, recency:monetary_value) %>%
+  write_csv("segmentation.csv")
 
-
+# plotting donors
+donors_long <- read_csv("plotting.csv")
+view(head(donors_long))
+donors_long %>%
+  ggplot(aes(x = Attribute, y = Value, group = cluster)) +
+  geom_line()
